4a883715899715779ce2e03669a81643c66bfe28
Josh Nowak <josh.nowak@speedgoat.io>
2020-04-22 13:00:51 -0600

    Initial commit
    Created repository, built package structure with use_this, created data object spdgt_pal, documented data and committed.

---
+++ R/colors.R
@@ -0,0 +1,2 @@
+# TODO Thinking colors to create gradients and subset palette
+# For reference look to viridis, viridis lite or similar packages
---
+++ R/spdgt_pal.R
@@ -0,0 +1,8 @@
+#' SpeedGoat color palette
+#'
+#' A vector of hex codes stored as data.
+#'
+#' @format A character vector of length 8
+#'
+#' @source Developed by SpeedGoat
+"spdgt_pal"
